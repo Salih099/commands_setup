53660c093f5d3f7188cfc2c9efe9a26ed1160130
Salih <pc.computer404@gmail.com>
2022-11-20 22:02:36 -0500

    changed US2

--- src/US/US2.java
+++ src/US/US2.java
@@ -3,7 +3,7 @@ package US;
 public class US2{
 
   public static void main(String[] args){
-    System.out.println("US2"); // changed the from ( system.out.println("US2"); ) - to - ( System.out.println("US2"); )
+    System.out.println("US2"); // changed the form ( system.out.println("US2"); ) - to - ( System.out.println("US2"); )
   }
 
 }
